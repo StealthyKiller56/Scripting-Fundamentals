49faa42d9d2ea13ab4a1af73122a0b86de7fee8c
StealthyKiller56 <jjo020409@gmail.com>
2023-10-10 13:59:46 -0700

    update code challenge

---
+++ Scripting Examples/Assets/Scripts/Script.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public string PlayerName = "Jaden";
+    public float GPA = 3.883f;
+    public int GradeLevel = 9;
+    public bool CanYouUseYourNotesOnTheQuiz = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
